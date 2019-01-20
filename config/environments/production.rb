3887b72a567d1357612271feb143b2353969d8c8
kyo <hamakyo711@gmail.com>
2019-01-20 10:09:21 +0900

    modified env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
  #sendgridの設定
  config.action_mailer.raise_delivery_errors = true
  config.action_mailer.delivery_method = :smtp
- host = 'safe-chamber-85664.herokuapp.com'
+ host = 'https://safe-chamber-85664.herokuapp.com'
  config.action_mailer.default_url_options = { host: host }
  ActionMailer::Base.smtp_settings = {
    :address        => 'smtp.sendgrid.net',
